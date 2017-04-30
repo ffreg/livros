789ec1e4c41a009622d44fbd1721840b39ab6f1a
ffreg <freguglia@outlook.com>
2017-04-30 02:31:54 -0300

    4rd commit to livros

---
+++ teste.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>Teste do PHP no Azure</title> 
+	</head>
+	<body> 
+		<?php echo '<p>Hello World !!</p>'; ?>
+		<?php
+		// Conexao SQL database 
+		$host = "tcp:meuslivros.database.windows.net,1433";
+		$user = "ffreg@meuslivros.database.windows.net";
+		$pwd = "Sistema53!";
+		$db = "livrosdb";
+ 
+		try {
+	   	 	$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+	    	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+ 
+		    $sqlQuery = $conn->query("SELECT * FROM [TABLE_NAME]");
+			$result = $sqlQuery->fetchAll();
+			echo "Row count: " . count($result) . ".";
+		}
+		catch(Exception $e){
+    		die(var_dump($e));
+		}
+		?>
+	</body>
+</html>
