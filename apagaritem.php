f4e7b1f3c9cdf83c7fc36930a5abba2c5981a813
ffreg <freguglia@outlook.com>
2017-04-30 16:50:41 -0300

    34.2 commit to livros

--- apagaritem.php
+++ apagaritem.php
@@ -72,25 +72,5 @@
 		</div>";	
 	}
 ?>
-
-    <div>
-     
-                <div>
-                    <div>
-                        <h3>Apagar um Livro<h3>
-                    </div>
-                </div> 
-                <div>      
-                    <form action="aoagaritem.php" method="post">
-                      <input type="hidden" name="id" value="<?php echo $id;?>"/>
-                      <p color=red>Tem certeza que deseja apagar o Livro ?</p>
-                      <div class="form-actions">
-                          <button type="submit">Sim</button>
-                          <a href="grid.php">Não - Voltar para o Grid</a>
-                      </div>
-                    </form>
-                </div>
-                 
-    </div>
   </body>
 </html>
