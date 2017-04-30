83824434080f74f9ddc924cbef66b4dac2988d2e
ffreg <freguglia@outlook.com>
2017-04-30 12:16:50 -0300

    19 commit to livros

---
+++ novoitem.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <div>
+		<div>
+			<div>
+            	<h3>Inserir um Novo Livro</h3>
+            </div>
+             
+                    <form action="novoitem.php" method="post">
+                      <div <?php echo !empty($nameError)?'erro':'';?>">
+                        <label>Nome</label>
+                        <div>
+                            <input name="nome" type="text"  placeholder="Nome" value="<?php echo !empty($name)?$name:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div>
+                          <button type="submit">Gravar</button>
+                          <a class="btn" href="grid.php">Voltar</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div>
+  </body>
+</html>
+        
