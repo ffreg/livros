4a36156a56be4e1f9b62baf3a1e039aa38328e53
ffreg <freguglia@outlook.com>
2017-04-30 16:30:19 -0300

    33 commit to livros

--- status.php
+++ status.php
@@ -10,7 +10,7 @@
 			if ( $tipo == 'N' ) { 
 			 	echo "Novo livro " . $_GET['nome'] . " foi gravado com sucesso!";
 			} elseif ( $tipo == 'U' ) {
-				echo "Livro " . $_GET['nome'] . " foi atualizado com sucesso!";
+				echo "Livro '" . $_GET['nome'] . "' foi atualizado com sucesso!";
 			}
 		?>
 	</div
