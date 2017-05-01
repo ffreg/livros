39972723309fb0634983306827d5a0504ffee169
ffreg <freguglia@outlook.com>
2017-05-01 17:57:31 -0300

    Added WebServices 3rd 1

--- webservices/index.php
+++ webservices/index.php
@@ -7,7 +7,7 @@ class RedeemAPI {
     
     // Dados da conexao - propriedades da classe
     private static $dbNome = 'livrosdb' ;
-    private static $dbServidor = 'tcp:meuslivros.database.windows.net,1433';
+    private static $dbServidor = '';
     private static $dbUsuario = 'ffreg@meuslivros.database.windows.net';
     private static $dbSenha = 'Sistema53!';
     
@@ -17,7 +17,7 @@ class RedeemAPI {
     
     	if ( null == self::$cont ) {     
     	    try {
-        	  self::$cont =  new PDO( "sqlsrv:Server=".self::$dbServidor.";"."Database=".self::$dbNome, self::$dbUsuario, self::$dbSenha); 
+        	  self::$cont =  new PDO( "sqlsrv:Server=tcp:meuslivros.database.windows.net,1433";"."Database=livrosdb", self::$dbUsuario, self::$dbSenha); 
           	// se fosse uma conexao com o MySQL a string de conexao seria: mysql:host ao inves de sqlsrv:Server= | dbname= as inves de Database="
        		}
         	catch(PDOException $e) {
@@ -36,12 +36,15 @@ class RedeemAPI {
 
     // Main method to redeem a code
     function redeem() {
+    		echo "Hello, PHP!";
+    
+    
         // Print all codes in database
-        $stmt = $this->db->prepare('SELECT id,nome FROM meuslivros');
-        $stmt->execute();
-        $stmt->bind_result($id, $nome);
-        while ($stmt->fetch()) {
-            echo "Passei por aqui - Debig Step 1";
+        //$stmt = $this->db->prepare('SELECT id,nome FROM meuslivros');
+        //$stmt->execute();
+        //$stmt->bind_result($id, $nome);
+        //while ($stmt->fetch()) {
+        //    echo "Passei por aqui - Debug Step 1";
         }
         $stmt->close();
     }
